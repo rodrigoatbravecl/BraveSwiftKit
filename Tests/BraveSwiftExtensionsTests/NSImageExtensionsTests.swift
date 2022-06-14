b7b8ff36f4ad54920347e822d6f3d701e555322d
Rodrigo Dutra de Oliveira <rodrigo@bravecl.com>
2022-06-14 00:15:06 -0300

    Remove BraveSwiftExtentionsTests cases

--- Tests/BraveSwiftExtensionsTests/NSImageExtensionsTests.swift
+++ Tests/BraveSwiftExtensionsTests/NSImageExtensionsTests.swift
@@ -2,24 +2,8 @@ import XCTest
 @testable import BraveSwiftExtensions
 
 final class NSImageExtensionsTests: XCTestCase {
-    var nsTestImage: NSImage? {
-        let url = Bundle.module.url(forResource: "YellowLabradorLooking_new", withExtension: "jpeg")
-        
-        guard let url = url else {
-            return nil
-        }
-        
-        return NSImage(byReferencing: url)
-    }
     
     func testExample() throws {
-        let converted = nsTestImage?.cgImage?.nsImage
-        let original = nsTestImage
-//        let d = ImageDiff()
-//        
-//        
-//        
-//        print(try! d.compare(leftImage: converted!.cgImage!, rightImage: original!.cgImage!))
         XCTAssertTrue(1 != 0)
     }
 }
